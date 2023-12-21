f34e127443149c328bae44dcc0009ff3c40974a2
SevanBadal <sgolnaza@gmail.com>
2023-12-20 20:18:31 -0500

    fe init

--- fe-app/src/App.tsx
+++ fe-app/src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pong, setPong] = useState<string>("...waiting for server")
+
+  const fetchPong = async () => {
+    const res = await fetch('http://localhost:8080/api/v1/ping')
+    const {data} = await res.json()
+    setPong(data)
+  }
+
+  useEffect(() => {
+    fetchPong()
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      Hello World
+      {
+        pong.length > 0 && (
+          <div>
+            {pong}
+          </div>
+        )
+      }
+
+    </main>
   )
 }
 
