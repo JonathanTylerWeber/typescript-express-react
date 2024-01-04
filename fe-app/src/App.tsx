8aa149116b37aabb7d3dafa42de3f404dce29fe3
SevanBadal <sgolnaza@gmail.com>
2024-01-04 17:12:04 -0500

    fe pulls and renders user data

--- fe-app/src/App.tsx
+++ fe-app/src/App.tsx
@@ -1,32 +1,49 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { User } from './models/User';
 
 function App() {
-  const [pong, setPong] = useState<string>("...waiting for server")
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
-  const fetchPong = async () => {
-    const res = await fetch('http://localhost:8080/api/v1/ping')
-    const {data} = await res.json()
-    setPong(data)
-  }
+  const fetchUsers = async () => {
+    setLoading(true);
+    setError('');
+    try {
+      const res = await fetch('http://localhost:8080/api/v1/users');
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const data = await res.json();
+      setUsers(data);
+    } catch (e) {
+      setError('Failed to fetch users');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    fetchPong()
-  },[])
+    fetchUsers();
+  }, []);
 
   return (
     <main>
-      Hello World
-      {
-        pong.length > 0 && (
-          <div>
-            {pong}
-          </div>
-        )
-      }
-
+      {loading && <div>Loading users...</div>}
+      {error && <div>Error: {error}</div>}
+      {users.length > 0 && (
+        <div>
+          <h2>Users:</h2>
+          <ul>
+            {users.map(user => (
+              <li key={user.id}>{`${user.firstName} ${user.lastName} (${user.email})`}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </main>
-  )
+  );
 }
 
 export default App
